aa612304ed3598c7eb8975b210159d69c1bb3242
malikkhalim <75404327+malikkhalim@users.noreply.github.com>
2025-03-05 08:48:34 +0000

    modified:   api/soap-proxy.js

--- api/soap-proxy.js
+++ api/soap-proxy.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     console.log(`Processing ${action} request`);
     
     // Use the correct SOAP endpoint (without ?wsdl)
-    const endpoint = 'http://demo-aerodili.nieve.id/wsdl.apiv12/index.php';
+    const endpoint = 'http://demo-aerodili.nieve.id/wsdl.apiv12/index.php?wsdl';
     
     // Set the correct SOAPAction header format
     const soapAction = `urn:sj_service#${action}`;
